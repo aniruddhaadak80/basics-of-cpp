4ff2c4c5fdc9ebea09ce876e87a71ffac241693b
Aniruddha Adak <adakaniruddha9@gmail.com>
2023-12-03 15:42:25 +0530

    new file:   c++/friendFunction.cpp         new file:   c++/friendFunction.exe         new file:   c++/maps.cpp

---
+++ c++/friendFunction.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// class Declearation
+class Home
+{
+private:
+    int id = 1;
+
+protected:
+    string name = "Aniruddha";
+
+public:
+    int favNum = 9;               // favouritre number
+    void NoramlFunction();        // Normal function
+    friend void friendFunction(); // decleration of friend function
+};
+
+// Description of friend function outside the class Home without Space resolution operator (::)
+void friendFunction()
+{
+    // object for friend function .It is only accessable inside friend functon...
+    Home obj; // you can not create an object inside the  main function for a  friend function
+
+    cout << "I am friend Functon in C++ " << endl;
+    // for privae data  member
+    cout << "I can access private data (like ID  ) :--> " << obj.id << endl; // 1
+    // for protected data member
+    cout << "I can also access protected data (like name  ) :--> " << obj.name << endl; // 9
+
+    // for public data member
+    cout << "I can obviously access public data (like favNum  ) :--> " << obj.favNum << endl; // 1
+}
+
+// Access and give description to the noraml function outside the class
+void Home ::NoramlFunction()
+{
+    Home noraml;
+    cout << "I am Normal Function " << endl;
+}
+
+// start of main function
+int main()
+{
+
+    friendFunction(); // call the friendFunction
+
+                      /*you cn not call
+                   normal function like :--->
+                      NoramlFunction();
+                      */
+
+    Home normal;
+    normal.NoramlFunction();
+}
